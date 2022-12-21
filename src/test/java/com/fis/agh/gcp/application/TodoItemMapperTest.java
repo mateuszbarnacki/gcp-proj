e5aa7db68d83e51e4b6d3e55dd5bf41ccdb98812
mateuszbarnacki <barnackimateusz@gmail.com>
2022-12-21 12:55:46 +0100

    Add unit tests

---
+++ src/test/java/com/fis/agh/gcp/application/TodoItemMapperTest.java
@@ -0,0 +1,77 @@
+package com.fis.agh.gcp.application;
+
+import com.fis.agh.gcp.domain.TodoItem;
+import com.google.cloud.Timestamp;
+import org.assertj.core.api.AssertionsForClassTypes;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+@SpringBootTest
+class TodoItemMapperTest {
+    @Autowired
+    private TodoItemMapper mapper;
+
+    @Test
+    void shouldMapDtoToEntity() {
+        TodoItemDto dto = givenTodoItemDto();
+
+        TodoItem entity = mapper.mapToEntity(dto);
+
+        AssertionsForClassTypes.assertThat(entity)
+                .hasFieldOrPropertyWithValue("title", "Test1")
+                .hasFieldOrPropertyWithValue("content", "Dummy content")
+                .hasFieldOrPropertyWithValue("address", "test@test.pl")
+                .hasFieldOrPropertyWithValue("completed", false);
+    }
+
+    @Test
+    void shouldMapEntityToDto() {
+        TodoItem entity = givenTodoItemEntity();
+
+        TodoItemDto dto = mapper.mapToDto(entity);
+
+        AssertionsForClassTypes.assertThat(entity)
+                .hasFieldOrPropertyWithValue("title", "Test2")
+                .hasFieldOrPropertyWithValue("content", "Empty")
+                .hasFieldOrPropertyWithValue("address", "test2@o2.pl")
+                .hasFieldOrPropertyWithValue("completed", false);
+    }
+
+    @Test
+    void shouldMapDate() {
+        Date date = new GregorianCalendar(2023, Calendar.APRIL, 1).getTime();
+
+        Timestamp timestamp = mapper.mapDateToGoogleTimestamp(date);
+
+        Assertions.assertEquals(0, timestamp.compareTo(Timestamp.of(date)));
+    }
+
+    private TodoItem givenTodoItemEntity() {
+        TodoItem item = new TodoItem();
+
+        item.setId(1L);
+        item.setTitle("Test2");
+        item.setContent("Empty");
+        item.setAddress("test2@o2.pl");
+        item.setDate(Timestamp.MIN_VALUE);
+        item.setCompleted(false);
+
+        return item;
+    }
+
+    private TodoItemDto givenTodoItemDto() {
+        return TodoItemDto.builder()
+                .title("Test1")
+                .content("Dummy content")
+                .address("test@test.pl")
+                .completed(false)
+                .date(new GregorianCalendar(2023, Calendar.APRIL, 1).getTime())
+                .build();
+    }
+}
---
+++ src/test/java/com/fis/agh/gcp/application/TodoItemRestServiceTest.java
@@ -0,0 +1,112 @@
+package com.fis.agh.gcp.application;
+
+import com.fis.agh.gcp.domain.TodoItem;
+import com.fis.agh.gcp.domain.TodoItemRepository;
+import com.google.cloud.Timestamp;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyBoolean;
+import static org.mockito.ArgumentMatchers.anyList;
+import static org.mockito.ArgumentMatchers.anyString;
+
+@ExtendWith(MockitoExtension.class)
+class TodoItemRestServiceTest {
+    private static final String EMAIL_ADDRESS = "test@test.pl";
+    private static final Date DATE = new GregorianCalendar(2023, Calendar.JANUARY, 13).getTime();
+    @Mock
+    private TodoItemRepository repository;
+    @Mock
+    private TodoItemMapper mapper;
+    @InjectMocks
+    private TodoItemRestService service;
+
+    @Test
+    void whenNullShouldThrowNullPointerException() {
+        Exception e = Assertions.assertThrows(NullPointerException.class, () -> service.queryTodoItems(null));
+        Assertions.assertNotNull(e);
+    }
+
+    @Test
+    void shouldReturnUserItem() {
+        QueryItemDto givenQueryItem = buildQueryItem();
+        Mockito.when(repository.getUserTodoItems(EMAIL_ADDRESS, false)).thenReturn(List.of(buildTodoItem()));
+        Mockito.when(mapper.mapToDto(buildTodoItem())).thenReturn(buildTodoItemDto());
+
+        List<TodoItemDto> items = service.getUserTodoItems(givenQueryItem);
+
+        Assertions.assertFalse(items.isEmpty());
+        Mockito.verify(repository).getUserTodoItems(anyString(), anyBoolean());
+    }
+
+    @Test
+    void shouldReturnListOfTodoItemsWhichShouldBeDone() {
+        QueryItemDto givenQueryItemWithDate = buildQueryItemWithDate();
+        Mockito.when(mapper.mapDateToGoogleTimestamp(DATE)).thenReturn(Timestamp.of(DATE));
+        Mockito.when(repository.getTodoItemsToBeDone(EMAIL_ADDRESS, Timestamp.of(DATE)))
+                .thenReturn(List.of(buildTodoItem()));
+        Mockito.when(mapper.mapToDto(buildTodoItem())).thenReturn(buildTodoItemDto());
+
+        service.getItemsWhichShouldBeDone(givenQueryItemWithDate);
+
+        Mockito.verify(repository).getTodoItemsToBeDone(anyString(), any());
+    }
+
+    @Test
+    void shouldDeleteAllUserTodoItems() {
+        QueryItemDto givenQueryItem = buildQueryItem();
+        Mockito.when(repository.getAllByAddress(EMAIL_ADDRESS)).thenReturn(List.of(buildTodoItem()));
+
+        service.deleteAllUserTodoItems(givenQueryItem);
+
+        Mockito.verify(repository).deleteAll(anyList());
+    }
+
+    private TodoItem buildTodoItem() {
+        TodoItem item = new TodoItem();
+
+        item.setId(1L);
+        item.setTitle("Test");
+        item.setContent("Dummy content");
+        item.setAddress(EMAIL_ADDRESS);
+        item.setDate(Timestamp.of(DATE));
+        item.setCompleted(false);
+
+        return item;
+    }
+
+    private TodoItemDto buildTodoItemDto() {
+        return TodoItemDto.builder()
+                .title("Test")
+                .content("Dummy content")
+                .address(EMAIL_ADDRESS)
+                .completed(false)
+                .date(DATE)
+                .build();
+    }
+
+    private QueryItemDto buildQueryItem() {
+        return QueryItemDto.builder()
+                .emailAddress(EMAIL_ADDRESS)
+                .completed(false)
+                .build();
+    }
+
+    private QueryItemDto buildQueryItemWithDate() {
+        return QueryItemDto.builder()
+                .emailAddress(EMAIL_ADDRESS)
+                .date(DATE)
+                .build();
+    }
+}
