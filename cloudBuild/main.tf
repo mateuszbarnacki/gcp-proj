f417d185ca818a9d2d1c991890e3bc1435085215
mateuszbarnacki <barnackimateusz@gmail.com>
2023-01-05 23:31:19 +0100

    Modify cloud build

--- cloudBuild/main.tf
+++ cloudBuild/main.tf
@@ -16,24 +16,6 @@ resource "google_cloudbuild_trigger" "cloud_build_trigger" {
     branch_name = var.branch_name
   }
 
-  build {
-    step {
-      name = "gcr.io/cloud-builders/docker"
-      args = ["build", "-t", "gcr.io/$PROJECT_ID/todolist-app", "."]
-    }
-
-    step {
-      name = "gcr.io/cloud-builders/docker"
-      args = ["push", "gcr.io/$PROJECT_ID/todolist-app"]
-    }
-
-    step {
-      name = "gcr.io/cloud-builders/gcloud"
-      args = ["run", "deploy", "app", "--image", "gcr.io/$PROJECT_ID/todolist-app", "--region", "europe-west3", "--platform", "managed", "--allow-unauthenticated"]
-    }
-
-  }
-
   filename   = var.cloud_build_filename
   depends_on = [google_project_service.build]
 }
