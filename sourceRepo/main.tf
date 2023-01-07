da2c1608e5fb310ce90dd348a4bfc4559665c8c2
mateuszbarnacki <barnackimateusz@gmail.com>
2023-01-07 17:46:12 +0100

    Change provisioners

--- sourceRepo/main.tf
+++ sourceRepo/main.tf
@@ -6,7 +6,4 @@ resource "google_project_service" "repo" {
 resource "google_sourcerepo_repository" "cloud-run-repo" {
   name       = var.cloud_run_repo_name
   depends_on = [google_project_service.repo]
-  /*provisioner "local-exec" {
-    command = "cd && cd gcp-proj/sourceRepo && rm -rf ${var.cloud_run_repo_name} && gcloud source repos clone ${var.cloud_run_repo_name} && cd ${var.cloud_run_repo_name} && cp -R ~/gcp-proj/cloudRun/* . && git add * && git commit -m 'Initial commit' && git push -u origin main"
-  }*/
 }
