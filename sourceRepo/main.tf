00cd886c5714d22c1bbb8520c371f31e142fccea
mateuszbarnacki <barnackimateusz@gmail.com>
2023-01-07 17:18:36 +0100

    Modify provisioners

--- sourceRepo/main.tf
+++ sourceRepo/main.tf
@@ -7,9 +7,6 @@ resource "google_sourcerepo_repository" "cloud-run-repo" {
   name       = var.cloud_run_repo_name
   depends_on = [google_project_service.repo]
   provisioner "local-exec" {
-    command = "pwd"
-  }
-  provisioner "local-exec" {
-    command = "cd /sourceRepo && rm -rf ${var.cloud_run_repo_name} && gcloud source repos clone ${var.cloud_run_repo_name} && cd ${var.cloud_run_repo_name} && cp -R ~/gcp-proj/cloudRun/* . && git add * && git commit -m 'Initial commit' && git push -u origin master"
+    command = "cd && cd gcp-proj/sourceRepo && rm -rf ${var.cloud_run_repo_name} && gcloud source repos clone ${var.cloud_run_repo_name} && cd ${var.cloud_run_repo_name} && cp -R ~/gcp-proj/cloudRun/* . && git add * && git commit -m 'Initial commit' && git push -u origin master"
   }
 }
