35486fdebb560777269cd6189f2301aa7697ec09
mateuszbarnacki <barnackimateusz@gmail.com>
2023-01-13 13:26:03 +0100

    Change TodoItem entity

--- src/main/java/com/fis/agh/gcp/domain/TodoItem.java
+++ src/main/java/com/fis/agh/gcp/domain/TodoItem.java
@@ -4,12 +4,15 @@ import com.google.cloud.Timestamp;
 import lombok.Data;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 @Data
 public class TodoItem {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String emailAddress;
     private String title;
     private String content;
