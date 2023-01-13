c4a4f72be5ec229c057dce80485593956f11da33
mateuszbarnacki <barnackimateusz@gmail.com>
2023-01-13 01:10:14 +0100

    Fix issues

--- cloudFunction/main.tf
+++ cloudFunction/main.tf
@@ -112,7 +112,7 @@ resource "google_cloudfunctions_function" "function" {
 
   event_trigger {
     event_type     = "providers/cloud.pubsub/eventTypes/topic.publish"
-    resource       = "projects/${var.project_id}/topics/${var.pubsub_topic_name}"
+    resource       = var.pubsub_topic_name
   }
 
   depends_on = [
