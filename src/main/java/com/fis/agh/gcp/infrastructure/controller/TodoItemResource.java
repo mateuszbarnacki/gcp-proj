5af540e2ca80471916500a922497467cafb839c7
mateuszbarnacki <barnackimateusz@gmail.com>
2023-01-20 08:57:16 +0100

    Change Swagger to OpenApi dependency

--- src/main/java/com/fis/agh/gcp/infrastructure/controller/TodoItemResource.java
+++ src/main/java/com/fis/agh/gcp/infrastructure/controller/TodoItemResource.java
@@ -1,24 +1,31 @@
 package com.fis.agh.gcp.infrastructure.controller;
 
 import com.fis.agh.gcp.application.TodoItemDto;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/item")
+@Tag(name = "TodoItem", description = "This resource handle todo item operations")
 public interface TodoItemResource {
 
+    @Operation(summary = "Publish PubSub message which trigger cloud function", responses = {
+            @ApiResponse(responseCode = "201", description = "Message published successfully"),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "The request was not valid.",
+                    content = @Content(
+                            examples = {@ExampleObject(
+                                    value = "{\"message\":\"Invalid email address structure!\", \"code\":\"BAD_REQUEST\"}"
+                            )},
+                            mediaType = MediaType.APPLICATION_JSON_VALUE))
+    })
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.TEXT_PLAIN_VALUE)
-    @ApiOperation(value = "Creates new TodoItem instance",
-            notes = "Method consumes JSON which contains TodoItem data.")
-    @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "OK"),
-            @ApiResponse(code = 201, message = "Item created successfully"),
-            @ApiResponse(code = 400, message = "Invalid TodoItem object"),
-            @ApiResponse(code = 500, message = "Could not save item in database")})
     ResponseEntity<String> createTodoItem(TodoItemDto dto);
 }
