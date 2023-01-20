26349ae681c9867c8397cb2bcc3b384886ee2c56
mateuszbarnacki <barnackimateusz@gmail.com>
2023-01-20 09:12:13 +0100

    ItemValidator refactor

--- src/main/java/com/fis/agh/gcp/application/ItemValidator.java
+++ src/main/java/com/fis/agh/gcp/application/ItemValidator.java
@@ -1,46 +1,68 @@
 package com.fis.agh.gcp.application;
 
+import org.apache.logging.log4j.util.Strings;
 import org.springframework.stereotype.Component;
 
 import java.time.Instant;
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Objects;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 @Component
 public class ItemValidator {
-    private final List<String> validationMessages;
+    private final List<Validating> validators;
 
     public ItemValidator() {
-        this.validationMessages = new ArrayList<>();
+        this.validators = List.of(new TitleValidator(),
+                new EmailAddressValidator(),
+                new DateValidator());
     }
 
     public ValidationResult validateTodoItem(TodoItemDto todoItem) {
-        this.validationMessages.clear();
-        validateTitle(todoItem.getTitle());
-        validateEmailAddress(todoItem.getAddress());
-        validateDate(todoItem.getDate());
-        return new ValidationResult(this.validationMessages);
+        return new ValidationResult(applyValidators(todoItem));
     }
 
-    private void validateTitle(String title) {
+    private List<String> applyValidators(TodoItemDto todoItem) {
+        return this.validators.stream()
+                .map(validating -> validating.validate(todoItem))
+                .filter(Strings::isNotBlank)
+                .collect(Collectors.toList());
+    }
+}
+
+interface Validating {
+    String validate(TodoItemDto dto);
+}
+
+class TitleValidator implements Validating {
+    @Override
+    public String validate(TodoItemDto dto) {
+        String title = dto.getTitle();
+        return validateTitle(title);
+    }
+
+    private String validateTitle(String title) {
         if (Objects.isNull(title) || title.isBlank()) {
-            this.validationMessages.add("Title could not be blank!");
+            return "Title could not be blank!";
         }
+        return Strings.EMPTY;
+    }
+}
+
+class EmailAddressValidator implements Validating {
+    @Override
+    public String validate(TodoItemDto dto) {
+        String emailAddress = dto.getAddress();
+        return validateEmailAddress(emailAddress);
     }
 
-    private void validateEmailAddress(String emailAddress) {
+    private String validateEmailAddress(String emailAddress) {
         if (!validateEmail(emailAddress)) {
-            this.validationMessages.add("Invalid email address structure!");
+            return "Invalid email address structure!";
         }
-    }
-
-    private void validateDate(Date date) {
-        if (date.compareTo(Date.from(Instant.now())) < 0) {
-            this.validationMessages.add("Given date is in the past!");
-        }
+        return Strings.EMPTY;
     }
 
     private boolean validateEmail(String emailAddress) {
@@ -49,5 +71,20 @@ public class ItemValidator {
         return Pattern.compile(regexPattern)
                 .matcher(emailAddress)
                 .matches();
+    }
+}
+
+class DateValidator implements Validating {
+    @Override
+    public String validate(TodoItemDto dto) {
+        Date date = dto.getDate();
+        return validateDate(date);
+    }
+
+    private String validateDate(Date date) {
+        if (date.compareTo(Date.from(Instant.now())) < 0) {
+            return "Given date is in the past!";
+        }
+        return Strings.EMPTY;
     }
 }
