fe912fbc1356046e870d5ac2cab037a0af01e3ce
mateuszbarnacki <barnackimateusz@gmail.com>
2023-01-20 09:46:31 +0100

    Revert Swagger changes

--- src/main/java/com/fis/agh/gcp/infrastructure/controller/TodoItemResource.java
+++ src/main/java/com/fis/agh/gcp/infrastructure/controller/TodoItemResource.java
@@ -1,31 +1,25 @@
 package com.fis.agh.gcp.infrastructure.controller;
 
 import com.fis.agh.gcp.application.TodoItemDto;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.ExampleObject;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/item")
-@Tag(name = "TodoItem", description = "This resource handle todo item operations")
 public interface TodoItemResource {
 
-    @Operation(summary = "Publish PubSub message which trigger cloud function", responses = {
-            @ApiResponse(responseCode = "201", description = "Message published successfully"),
-            @ApiResponse(
-                    responseCode = "400",
-                    description = "The request was not valid.",
-                    content = @Content(
-                            examples = {@ExampleObject(
-                                    value = "{\"message\":\"Invalid email address structure!\", \"code\":\"BAD_REQUEST\"}"
-                            )},
-                            mediaType = MediaType.APPLICATION_JSON_VALUE))
-    })
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.TEXT_PLAIN_VALUE)
+    @ApiOperation(value = "Publish Pubsub message which triggers cloud function",
+            notes = "Method consumes JSON which contains TodoItem data.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "OK"),
+            @ApiResponse(code = 201, message = "Item published successfully"),
+            @ApiResponse(code = 400, message = "Invalid TodoItem object"),
+            @ApiResponse(code = 500, message = "Could not publish item")})
     ResponseEntity<String> createTodoItem(TodoItemDto dto);
 }
