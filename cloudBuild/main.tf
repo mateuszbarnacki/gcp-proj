56b82d79113367359e978d697a3cefdf61b6011c
mateuszbarnacki <barnackimateusz@gmail.com>
2023-01-20 02:12:24 +0100

    Remove cloud build step

--- cloudBuild/main.tf
+++ cloudBuild/main.tf
@@ -32,11 +32,6 @@ resource "google_cloudbuild_trigger" "cloud_build_trigger" {
       name = "gcr.io/cloud-builders/docker"
       args = ["push", "gcr.io/${var.project_id}/todolist-app"]
     }
-
-    /*step {
-      name = "gcr.io/cloud-builders/gcloud"
-      args = ["run", "deploy", "gcp-proj-app", "--image", "gcr.io/${var.project_id}/todolist-app", "--region", "europe-west3", "--platform", "managed", "--allow-unauthenticated"]
-    }*/
   }
 }
 
